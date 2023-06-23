f6ff27a6c76d35e1c47f46526551fd711df6e819
gnab <gnabaev@gmail.com>
2023-06-23 21:18:07 +0300

    Creation of the game field. Realisation of the pushing Left and Right buttons.

--- 2048Game/MainForm.Designer.cs
+++ 2048Game/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _2048Game
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,35 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(117, 164);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 70);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "2048";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "2048 The Game";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ 2048Game/MainForm.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048Game
+{
+    public partial class MainForm : Form
+    {
+        private const int mapSize = 4;
+        private Label[,] labelsMap;
+        private static Random random = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            InitMap();
+            GenerateNumber();
+
+        }
+
+        private void GenerateNumber()
+        {
+            while (true)
+            {
+                var labelNumber = random.Next(mapSize * mapSize);
+                var rowIndex = labelNumber / mapSize;
+                var rowColumn = labelNumber % mapSize;
+
+                if (labelsMap[rowIndex, rowColumn].Text == string.Empty)
+                {
+                    labelsMap[rowIndex, rowColumn].Text = "2";
+                    break;
+                }
+            }
+        }
+
+        private void InitMap()
+        {
+            labelsMap = new Label[mapSize, mapSize];
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    var newLabel = CreateLabel(i, j);
+                    Controls.Add(newLabel);
+                    labelsMap[i, j] = newLabel;
+                }
+
+            }
+        }
+
+        private Label CreateLabel(int rowIndex, int columnIndex)
+        {
+            var label = new Label();
+            label.BackColor = SystemColors.ControlLight;
+            label.Font = new Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            label.Size = new Size(70, 70);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Location = new Point(10 + columnIndex * 75, 10 + rowIndex * 75);
+            return label;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = mapSize - 1; j >= 0; j--)
+                    {
+                        if (labelsMap[i, j].Text != string.Empty)
+                        {
+                            for (int k = j - 1; k >= 0; k--)
+                            {
+                                if (labelsMap[i, k].Text != string.Empty)
+                                {
+                                    if (labelsMap[i, j].Text == labelsMap[i, k].Text)
+                                    {
+                                        var number = int.Parse(labelsMap[i, j].Text);
+                                        labelsMap[i, j].Text = (number * 2).ToString();
+                                        labelsMap[i, k].Text = string.Empty;
+                                    }
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = mapSize - 1; j >= 0; j--)
+                    {
+                        if (labelsMap[i, j].Text == string.Empty)
+                        {
+                            for (int k = j - 1; k >= 0; k--)
+                            {
+                                if (labelsMap[i, k].Text != string.Empty)
+                                {
+                                    labelsMap[i, j].Text = labelsMap[i, k].Text;
+                                    labelsMap[i, k].Text = string.Empty;
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        if (labelsMap[i, j].Text != string.Empty)
+                        {
+                            for (int k = j + 1; k < mapSize; k++)
+                            {
+                                if (labelsMap[i, k].Text != string.Empty)
+                                {
+                                    if (labelsMap[i, j].Text == labelsMap[i, k].Text)
+                                    {
+                                        var number = int.Parse(labelsMap[i, j].Text);
+                                        labelsMap[i, j].Text = (number * 2).ToString();
+                                        labelsMap[i, k].Text = string.Empty;
+                                    }
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        if (labelsMap[i, j].Text == string.Empty)
+                        {
+                            for (int k = j + 1; k < mapSize; k++)
+                            {
+                                if (labelsMap[i, k].Text != string.Empty)
+                                {
+                                    labelsMap[i, j].Text = labelsMap[i, k].Text;
+                                    labelsMap[i, k].Text = string.Empty;
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            GenerateNumber();
+        }
+    }
+}
